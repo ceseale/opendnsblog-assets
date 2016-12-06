8b67b0fcce81e5fdc560dd707ed795dd8a348fac
Colin Seale <cseale@opendns.com>
2016-12-06 14:31:58 -0800

    setup for blog on do domains

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Graph from './components/graph/graph';
+// import Graph from './components/graph/graph';
 import ForceGraph from './components/forceGraph/ForceDirectedGraph';
 import GlobePatterns from './components/GlobePatterns/GlobePatterns';
-import data from './data.json';
+import data from './sednettoptop.json';
 import globalStyle from './global.scss';
-import JSONTree from 'react-json-tree';
+// import JSONTree from 'react-json-tree';
 
 
 
@@ -34,25 +34,15 @@ let App = {
 
         const getItemString = (type, data, itemType, itemString) => (<span>{ (data.type || data.depth) || itemString }</span>);
         // render aplication
-        ReactDOM.render(
-            <JSONTree getItemString={getItemString} hideRoot={true} data={data} />,
-            document.getElementById('tree-container')
-        );
+        for (let depth = 0; depth <= 5; depth++) {   
+          console.log(depth);     
+          ReactDOM.render(
+              <ForceGraph width={760} height={340} depth={depth} data={data} />,
+              document.getElementById(`fgraph-container${depth}`)
+          );
+        }
 
-        ReactDOM.render(
-            <Graph width={760} height={340} codeRunCount={0} data={data} />,
-            document.getElementById('graph-container')
-        );
 
-        ReactDOM.render(
-            <ForceGraph width={760} height={340} codeRunCount={0} data={data} />,
-            document.getElementById('fgraph-container')
-        );
-
-        ReactDOM.render(
-            <GlobePatterns width={760} height={760} codeRunCount={0} data={data} />,
-            document.getElementById('globe-container')
-        );
     }
 };
 
